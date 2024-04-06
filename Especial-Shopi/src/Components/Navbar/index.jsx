b897e801dc42beb3e69c89e38ac07fd99cc85741
KGabriel Perez <121875673+KennyGi0053@users.noreply.github.com>
2024-04-06 11:07:44 -0300

    actualizacion de nombres

--- Especial-Shopi/src/Components/Navbar/index.jsx
+++ Especial-Shopi/src/Components/Navbar/index.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <ul className="flex items-center gap-3">
             <li className="font-semibold text-lg">
                 <NavLink to='/'> 
-                Especial Travel
+                Especial Shopi
                 </NavLink>
             </li>
             <li>
